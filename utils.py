428d31b9443b12914cafc013dadf95c8a8e84969
daniiltesluk <djskeletik7@gmail.com>
2023-07-12 23:16:53 +0200

    initial code 2.0

--- utils.py
+++ utils.py
@@ -1,11 +1,5 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
-# class ST (StatesGroup):
-#     Menu = State()
-#     Buyer = State()
-#     Salesman = State()
-#     Applications = State()
-
 class ST(StatesGroup):
     Menu = State("Menu")
     Salesman = State("Salesman")
