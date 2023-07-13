d4a4c1f157db9805e6e8fbe9f16237f7368e0218
HyperPC <maslovvasiliy99@gmail.com>
2023-07-13 15:27:50 +0300

    add user

--- bot_main.py
+++ bot_main.py
@@ -3,9 +3,13 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram import Bot, Dispatcher, types
 from aiogram.dispatcher import FSMContext
 from aiogram import executor
+from config import DB_CONFIG
+from DataBase import DB
 from utils import ST
 import logging
+import secrets
 import config
+
 
 # Define menu options
 MENU_START_OPTIONS = ["Продавец", "Покупатель"]
@@ -17,6 +21,7 @@ bot = Bot(token=config.BOT_TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 dp.middleware.setup(LoggingMiddleware())
 logging.basicConfig(level=logging.INFO)
+db = DB(DB_CONFIG['host'], DB_CONFIG['user'], DB_CONFIG['password'], DB_CONFIG['db'])
 
 # Create reply keyboard markup for menus
 def create_menu_markup(options):
@@ -32,7 +37,12 @@ MENU_APPLICATIONS = create_menu_markup(MENU_APPLICATIONS_OPTIONS)
 
 @dp.message_handler(commands="start")
 async def start(message: types.Message, state: FSMContext):
-    await message.reply("Привет", reply_markup=MENU_START)
+    if db.get_user_by_param("username", message.from_user.username):
+        await message.reply(f"С возвращением, {message.from_user.username}", reply_markup=MENU_START)
+    else:
+        db.add_user("Bruh", message.from_user.id, message.from_user.username)
+        await message.reply(f"Мы рады вас приветствовать, {message.from_user.username}", reply_markup=MENU_START)
+
     await ST.Menu.set()
 
 @dp.message_handler(state=ST.Menu)
@@ -52,10 +62,9 @@ async def menu(message: types.Message, state: FSMContext):
 @dp.message_handler(state=ST.Buyer)
 async def menu_buyer(message: types.Message):
     text = message.text
-
     if text == "Зарегать заявку":
-        await message.reply("Скиньте ссылку на товар", reply_markup=MENU_BUYER)
-        await ST.Applications.set()
+        await message.reply("Скиньте ссылку на товар")
+        await ST.Application_register.set()
     elif text == "Выйти":
         await message.reply("Вы вышли из раздела 'Покупатель'", reply_markup=MENU_START)
         await ST.Menu.set()
@@ -76,11 +85,10 @@ async def menu_salesman(message: types.Message):
         await message.reply("Что-то непонятное вводишь", reply_markup=MENU_SALESMAN)
         await ST.Salesman.set()
 
-@dp.message_handler(state=ST.Applications)
-async def menu_applications(message: types.Message):
-    text = message.text
-    if text == "Выйти":
-        await message.reply("Вы вышли из раздела 'Покупатель'", reply_markup=MENU_START)
-        await ST.Menu.set()
+# @dp.message_handler(state=ST.Application_register)
+# async def menu_applications(message: types.Message):
+#     text = message.text
+#     token = secrets.token_hex(16)
+#     db.add_thing(token, "item_type", "item_name", "manufacturer", 100, "delivery_markup", customer, item_link, description=None)
 
 executor.start_polling(dp, skip_updates=True)
--- utils.py
+++ utils.py
@@ -4,4 +4,7 @@ class ST(StatesGroup):
     Menu = State("Menu")
     Salesman = State("Salesman")
     Buyer = State("Buyer")
-    Applications = State("Applications")
+    Application_viewer = State("Application_viewer")
+    Application_register = State("Application_register")
+    Applications_Buyer = State("Applications_Buyer")
+    Applications_Salesman = State("Applications_Salesman")
